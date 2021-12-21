98227d9254df7dd827f41cbcfdd32817003030f4
Tiago <ottiagos@gmail.com>
2021-12-21 20:19:22 -0300

    Before tests refactor

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -45,4 +45,38 @@ describe('AppointMentsDayView', () => {
         expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
     });
 
+    it('renders multiple appointments in a on element', () => {
+        const today = new Date();
+        const appointments = [
+            { startsAt: today.setHours(12, 0) },
+            { startsAt: today.setHours(13, 0) },
+        ];
+
+        render(<AppointmentsDayView appointments={appointments} />);
+
+        expect(container.querySelector('ol')).not.toBeNull();
+        expect(container.querySelector('ol').children).toHaveLength(2);
+
+    });
+
+    it('renders each appointment in an li', () => {
+        const today = new Date();
+        const appointments = [
+            { startsAt: today.setHours(12, 0) },
+            { startsAt: today.setHours(13, 0) },
+        ];
+
+        render(<AppointmentsDayView appointments={appointments} />);
+
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+
+        expect(
+            container.querySelectorAll('li')[0].textContent)
+            .toEqual('12:00');
+
+        expect(
+            container.querySelectorAll('li')[1].textContent)
+            .toEqual('13:00');
+    });
 });
+
