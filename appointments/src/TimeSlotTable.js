f6253a4fd661f1ed2b9ffc00090d8d7d5746878d
Tiago <ottiagos@gmail.com>
2022-01-03 13:37:19 -0300

    Refactor

--- appointments/src/TimeSlotTable.js
+++ appointments/src/TimeSlotTable.js
@@ -1,28 +1,29 @@
 import React from "react";
 
+
+const timeIncrements = (numTimes, startTime, increment) =>
+    Array(numTimes)
+        .fill([startTime])
+        .reduce((acc, _, i) =>
+            acc.concat([startTime + (i * increment)]));
+
 const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
     const totalSlots = (salonClosesAt - salonOpensAt) * 2;
     const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
     const increment = 30 * 60 * 1000;
-    return Array(totalSlots)
-        .fill([startTime])
-        .reduce((acc, _, i) =>
-            acc.concat([startTime + (i * increment)])
-        );
-};
 
-const toTimeValue = timestamp =>
-    new Date(timestamp).toTimeString().substring(0, 5);
+    return timeIncrements(totalSlots, startTime, increment);
+};
 
 const weeklyDateValues = (startDate) => {
     const midnight = new Date(startDate).setHours(0, 0, 0, 0);
     const increment = 24 * 60 * 60 * 1000;
-    return Array(7)
-        .fill([midnight])
-        .reduce((acc, _, i) =>
-            acc.concat([midnight + (i * increment)])
-        );
+    return timeIncrements(7, midnight, increment);
 };
+
+
+const toTimeValue = timestamp =>
+    new Date(timestamp).toTimeString().substring(0, 5);
 
 const toShortDate = timestamp => {
     const [day, , dayOfMonth] = new Date(timestamp)
