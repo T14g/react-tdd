3d921a53f89335bd83b4ae89d4820b40a9e83f95
Tiago <ottiagos@gmail.com>
2022-05-03 23:38:08 -0300

    test if fetches customer data

--- appointments/src/CustomerSearch.js
+++ appointments/src/CustomerSearch.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 export const CustomerSearch = () => {
+    //use the same useEffect ceremony that we've seen before, using an
+    // inline function to ensure we don't return a value to useEffect, and passing an
+    // empty array to ensure the effect only runs when the component is first mounted:
+
+    useEffect(() => {
+        const fetchData = () =>
+            window.fetch('/customers', {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: { 'Content-Type': 'application/json' }
+            });
+        fetchData();
+    }, []);
+
     return (
         <table>
             <thead>
--- appointments/test/CustomerSearch.test.js
+++ appointments/test/CustomerSearch.test.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { createContainer } from "./domManipulators";
 import { CustomerSearch } from '../src/CustomerSearch';
+import { fetchResponseOk } from "./spyHelpers";
+import 'whatwg-fetch';
 
 describe('CustomerSearch', () => {
     let renderAndWait, elements;
 
     beforeEach(() => {
         ({ renderAndWait, elements } = createContainer());
+
+        jest.spyOn(window, 'fetch');
     });
 
     it('renders a table with four headings', async () => {
@@ -16,4 +20,16 @@ describe('CustomerSearch', () => {
             'First name', 'Last name', 'Phone number', 'Actions'
         ]);
     });
+
+    it('fetches all customer data when component mounts', async () => {
+        await renderAndWait(<CustomerSearch />);
+
+        // You need a mock for window.fetch for this to work 
+        expect(window.fetch).toHaveBeenCalledWith('/customers', {
+            method: 'GET',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json' }
+        });
+    });
+
 });
